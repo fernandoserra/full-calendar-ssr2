f162ef4923f98ef9d45442d525ee3a6f0907fe10
Fernando Serra <fernando.serra.r@gmail.com>
2022-05-30 20:46:37 -0400

    update calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,Inject } from '@angular/core';
+
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+
+var FullCalendar:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'full-calendar-ssr2';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  
+  ngOnInit() {
+
+    if (isPlatformBrowser(this.platformId)) {
+      window.onload = function(){
+      var link=document.createElement('link');
+      link.href='https://unpkg.com/@fullcalendar/core@4.4.0/main.min.css';
+      link.rel='stylesheet';
+
+      var link2=document.createElement('link');
+      link2.href='https://unpkg.com/@fullcalendar/daygrid@4.4.0/main.min.css';
+      link2.rel='stylesheet';
+
+      var script3=document.createElement('script');
+      script3.src='https://unpkg.com/@fullcalendar/core@4.4.0/main.min.js';
+  
+
+      var script4=document.createElement('script');
+      script4.src='https://unpkg.com/@fullcalendar/daygrid@4.4.0/main.min.js';
+
+      var script=document.createElement("script");
+      script.type="text/javascript";
+      script.innerHTML=" document.addEventListener('DOMContentLoaded', function() { var calendarEl = document.getElementById('calendar');  var calendar = new FullCalendar.Calendar(calendarEl, {plugins: [ 'dayGrid' ], timeZone: 'UTC', defaultView: 'dayGridMonth', events: 'https://fullcalendar.io/demo-events.json'}); calendar.render(); }); ";
+ 
+
+      document.getElementsByTagName('head')[0].appendChild(link);
+      document.getElementsByTagName('head')[0].appendChild(link2);
+      document.getElementsByTagName('head')[0].appendChild(script3);
+      document.getElementsByTagName('head')[0].appendChild(script4);
+      document.getElementsByTagName('head')[0].appendChild(script);
+
+      var iDiv = document.createElement('div');
+      iDiv.id = 'calendar';
+      iDiv.className = 'calendar';
+      document.getElementsByTagName('body')[0].appendChild(iDiv);
+
+      console.log("ejecutando")
+    }
+     /* setTimeout(() => {
+        console.log("ejecutnadoodjsdfn")
+        document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });*/
+    
+     // }, 5000);
+    }
+
+
+    
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+//import { FullCalendarModule } from '@fullcalendar/angular';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    //FullCalendarModule // for FullCalendar!
   ],
   providers: [],
   bootstrap: [AppComponent]
